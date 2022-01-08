cf4053e7359f23143eab062637327f3b55e03869
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2022-01-08 11:52:33 +0100

    Kitty Ubuntu Mono font

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,7 @@
 ;;; PACKAGE LIST
 (setq package-archives 
       '(("melpa" . "https://melpa.org/packages/")
+	("org" . "https://orgmode.org/elpa/")
         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 ;;; BOOTSTRAP USE-PACKAGE
@@ -18,21 +19,73 @@
 
 
 
+(setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(set-frame-font "Ubuntu Mono 14" nil t)
+(set-frame-font "Ubuntu Mono 15" nil t)
 
-(use-package vertico
-  :config
-  (vertico-mode))
- 
 (use-package gruvbox-theme
   :config
   (load-theme 'gruvbox-dark-hard t))
 
+(use-package command-log-mode)
 
+
+
+;;; IVY
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel)
+(use-package swiper)
+(use-package flx) 
+
+; fuzzy search
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-fuzzy)))
+
+
+;;; END IVY
+
+
+
+;;; LSP MODE
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l"))
+  ; :config
+  ; (lsp-enable-which-key-integration t))
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 4))
+
+;;; END LSP MODE
 
 ;;; END OTHER ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -62,3 +115,5 @@
   :config
   (setq evil-want-integration t)
   (evil-collection-init))
+
+;;; END
