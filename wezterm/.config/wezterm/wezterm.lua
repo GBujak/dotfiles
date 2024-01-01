5db5caa8ce0f9fc44f1be72a5681240c5030d53b
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2024-01-01 21:24:13 +0100

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require 'wezterm';
 
 return {
-    font = wezterm.font("Fira Code"),
-    color_scheme = "Gruvbox Dark",
+    font = wezterm.font("Go Mono"),
+    color_scheme = "Alabaster",
     hide_tab_bar_if_only_one_tab = true,
-    font_size = 14.0,
+    font_size = 18.0,
 
     keys = {
         { key = "\"", mods = "CTRL", action = {SplitVertical={domain="CurrentPaneDomain"}} },
